97f218744762776ceb7a16af6643486d1f37b562
devheitt <agustin_cristian.heitt@hotmail.com>
2021-02-14 11:32:10 -0300

    Se cambian los numeros magicos por constantes

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,10 @@ class GildedRose {
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final int MAX_QUALITY = 50;
+    public static final int BACKSTAGE_DOUBLE_QUALITY_INCREASE_SELLIN = 10;
+    public static final int BACKSTAGE_TRIPLE_QUALITY_INCREASE_SELLIN = 5;
+    public static final int MINIMUN_QUALITY = 0;
 
     Item[] items;
 
@@ -12,35 +16,41 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item itemTmp = items[i];
-            switch (itemTmp.name) {
-                case AGED_BRIE:
-                    itemTmp.sellIn--;
-                    if(itemTmp.quality < 50)
-                        itemTmp.quality++;
-                    if(itemTmp.sellIn < 0 && itemTmp.quality < 50)
-                        itemTmp.quality++;
-                    break;
-                case BACKSTAGE_PASSES:
-                    if(itemTmp.quality < 50)
-                        itemTmp.quality++;
-                    if(itemTmp.sellIn <= 10 && itemTmp.quality < 50)
-                        itemTmp.quality++;
-                    if(itemTmp.sellIn <= 5 && itemTmp.quality < 50)
-                        itemTmp.quality++;
-                    break;
-                case SULFURAS_HAND_OF_RAGNAROS:
-                    break;
-                default:
-                    itemTmp.sellIn--;
-                    if(itemTmp.quality > 0)
-                        itemTmp.quality--;
+        for (Item item : items)
+            update(item);
 
-                    if(itemTmp.sellIn < 0 && itemTmp.quality > 0)
-                        itemTmp.quality--;
-            }
+    }
 
+    private void update(Item itemTmp) {
+        switch (itemTmp.name) {
+            case AGED_BRIE:
+                reduceSellIn(itemTmp);
+                if(itemTmp.quality < MAX_QUALITY)
+                    itemTmp.quality++;
+                if(itemTmp.sellIn < 0 && itemTmp.quality < MAX_QUALITY)
+                    itemTmp.quality++;
+                break;
+            case BACKSTAGE_PASSES:
+                if(itemTmp.quality < MAX_QUALITY)
+                    itemTmp.quality++;
+                if(itemTmp.sellIn <= BACKSTAGE_DOUBLE_QUALITY_INCREASE_SELLIN && itemTmp.quality < MAX_QUALITY)
+                    itemTmp.quality++;
+                if(itemTmp.sellIn <= BACKSTAGE_TRIPLE_QUALITY_INCREASE_SELLIN && itemTmp.quality < MAX_QUALITY)
+                    itemTmp.quality++;
+                break;
+            case SULFURAS_HAND_OF_RAGNAROS:
+                break;
+            default:
+                reduceSellIn(itemTmp);
+                if(itemTmp.quality > MINIMUN_QUALITY)
+                    itemTmp.quality--;
+
+                if(itemTmp.sellIn < 0 && itemTmp.quality > MINIMUN_QUALITY)
+                    itemTmp.quality--;
         }
+    }
+
+    private void reduceSellIn(Item itemTmp) {
+        itemTmp.sellIn--;
     }
 }
